68e213bc6f22ca97cf1d8e6ed734f07cace726d4
bluis9098 <bluis@hazelwick.org.uk>
2026-09-16 12:16:38 +0000

    16/09/26

---
+++ python/12E / Cp2/strings.py
@@ -0,0 +1,10 @@
+fname = input("Enter your first name:")
+lname = input("Enter your last name:")
+
+initial = fname[0]
+part2 = lname[0:4]
+length = len(fname) + len(lname)
+
+username = (initial + part2 + str(length)).lower()
+
+print("The username is:",username)
